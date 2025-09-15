bf4e9a94d18257dc6b4e8d55c0f87b8cefa17bd7
PingoLeon <git.staleness589@passmail.com>
2025-09-15 11:45:15 +0200

    feat: nodemon added

--- lab2/index.js
+++ lab2/index.js
@@ -1,6 +1,3 @@
-// Import a module
-const http = require('http')
-
 // Define a string constant concatenating strings
 const content = '<!DOCTYPE html>' +
 '<html>' +
@@ -14,25 +11,11 @@ const content = '<!DOCTYPE html>' +
 '    </body>' +
 '</html>'
 
-const url = require('url')
-const qs = require('querystring')
 
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end()
-}
+// ./index.js
+const http = require('http')
+const handles = require('./handles')
 
 http
-.createServer(serverHandle)
+.createServer(handles.serverHandle)
 .listen(8080)
