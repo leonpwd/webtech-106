b18f9b8a43f0a02a06a38d389db75840fa78d270
PingoLeon <git.staleness589@passmail.com>
2025-09-15 10:57:38 +0200

    feat: HTTP server (functional)

--- lab2/index.js
+++ lab2/index.js
@@ -1,3 +1,14 @@
-console.log("Hello Node.js!")
+// Import a module
+const http = require('http')
 
-console.log("Signing activé !");
+// Declare an http server
+http.createServer(function (req, res) {
+
+  // Write a response header
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+
+  // Write a response content
+  res.end('Hello World\n')
+
+// Start the server
+}).listen(8080)
