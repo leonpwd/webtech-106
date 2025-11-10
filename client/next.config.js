0d76cc16738c71909af9e572cc14b379bdd9c296
PingoLeon <git.staleness589@passmail.com>
2025-11-10 11:13:22 +0100

    fix: vercel changes

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,20 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
